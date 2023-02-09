7b4660516559b1f764924dc345e804491063e9e3
kuro-xx <k.kuroda@miwaza.co.jp>
2023-02-09 12:31:10 +0900

    feat: MyButtonの機能を拡張

--- src/kernel/components/elements/button/MyButton/createStyles.ts
+++ src/kernel/components/elements/button/MyButton/createStyles.ts
@@ -1,12 +1,42 @@
-import { css } from '@emotion/react';
+import { css, Theme } from '@emotion/react';
 
-export const createStyles = () => ({
+export type ColorOption = 'primary' | 'secondary';
+export type VariantOption = 'contained';
+
+export const createStyles = (theme: Theme, color?: ColorOption) => ({
   root: css({
     display: 'inline-block',
-    padding: '0.5em 1em',
-    border: '1px solid #000',
+    minWidth: 128,
+    padding: '1em 1.5em',
+    border: '1px solid transparent',
     borderRadius: '5px',
     backgroundColor: 'transparent',
     cursor: 'pointer',
+
+    '&:hover': {
+      textDecoration: 'none',
+    },
   }),
+  variant: {
+    default: css({
+      borderColor: color ? theme.color[color].dark.code : theme.color.text.code,
+      color: color ? theme.color[color].dark.code : theme.color.text.code,
+      transition: `background-color ${theme.transition.ease}, border-color ${theme.transition.ease}, color ${theme.transition.ease}`,
+
+      '&:hover': {
+        backgroundColor: color ? theme.color[color].dark.code : theme.color.text.code,
+        color: theme.color.contrastText.code,
+      },
+    }),
+    contained: css({
+      borderColor: color ? theme.color[color].main.code : theme.color.text.code,
+      backgroundColor: color ? theme.color[color].main.code : theme.color.text.code,
+      color: theme.color.contrastText.code,
+      transition: `opacity ${theme.transition.ease}`,
+
+      '&:hover': {
+        opacity: 0.8,
+      },
+    }),
+  },
 });
--- src/kernel/components/elements/button/MyButton/index.tsx
+++ src/kernel/components/elements/button/MyButton/index.tsx
@@ -1,9 +1,54 @@
-import { createStyles } from './createStyles';
+import { useTheme } from '@emotion/react';
+import Link from 'next/link';
+import { ColorOption, createStyles, VariantOption } from './createStyles';
 
-type Props = {
+type ButtonType = 'button' | 'submit';
+type LinkType = 'link' | 'external-link';
+
+type CommonProps = {
   children: React.ReactNode;
+  variant?: VariantOption;
+  color?: ColorOption;
 };
-export const MyButton = ({ children }: Props) => {
-  const styles = createStyles();
-  return <button css={styles.root}>{children}</button>;
+type ButtonProps = CommonProps & {
+  type: ButtonType;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+};
+type LinkProps = CommonProps & {
+  type: LinkType;
+  href: string;
+  onClick?: (e: React.MouseEvent<HTMLAnchorElement>) => void;
+};
+type Props = ButtonProps | LinkProps;
+
+export const MyButton = (props: Props) => {
+  const { children, variant, color, type } = props;
+  const theme = useTheme();
+  const styles = createStyles(theme, color);
+
+  if (type === 'link' || type === 'external-link') {
+    const { href, onClick } = props;
+    return (
+      <Link href={href} passHref>
+        <a
+          css={[styles.root, styles.variant[variant ?? 'default']]}
+          onClick={onClick}
+          target={type !== 'link' ? '_self' : '_blank'}
+        >
+          {children}
+        </a>
+      </Link>
+    );
+  }
+
+  if (type === 'button' || type === 'submit') {
+    const { onClick } = props;
+    return (
+      <button type={type} css={[styles.root, styles.variant[variant ?? 'default']]} onClick={onClick}>
+        {children}
+      </button>
+    );
+  }
+
+  return null;
 };
